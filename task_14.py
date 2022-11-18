e402de1e1f042d56d5f0ab3e735ee3816666a828
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-11-18 13:29:37 +0200

    Python1

---
+++ task_14.py
@@ -0,0 +1,16 @@
+# Напишите инструкции черепашьей графики, чтобы нарисовать квадрат со стороной
+# 100 пикселов и заполненный синим цветом.
+
+import turtle
+turtle.showturtle()
+
+turtle.fillcolor('blue')
+turtle.begin_fill()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.end_fill()
---
+++ task_15.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.showturtle()
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.fillcolor('red')
+turtle.begin_fill()
+turtle.circle(80)
+turtle.end_fill()
